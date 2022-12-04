05a35302b0b1d2e14706cbd0b7a140477c980a95
kkukileon305 <kkukileon305@gmail.com>
2022-12-04 17:47:46 +0900

    폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,16 @@ import '../styles/globals.css';
 import Header from '../components/header/Header';
 import Providers from '../components/Providers';
 
+import { Noto_Sans_KR } from '@next/font/google';
+
+const notosans = Noto_Sans_KR({
+  weight: ['300', '700'],
+  subsets: ['korean'],
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html className={notosans.className}>
       <head />
       <body className='min-h-[100vh] bg-white dark:bg-gray-700'>
         <Providers>
--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
+    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['korean'] } }],
   },
 };
 
