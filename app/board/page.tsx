a33f0f442d7894e22a72977e20a2ed8ea01bd028
kkukileon305 <kkukileon305@gmail.com>
2022-12-10 10:36:07 +0900

    searchparams hook으로 변경

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -8,6 +8,7 @@ import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 import { urlToTitle } from '../../lib/categories';
 import { BoardWithComment } from '../../components/Item';
 import { useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 const getBoards = async (pageParam = 1, categoryName: string): Promise<BoardWithComment[]> => {
   const res = await fetch(`/api/board?categoryName=${categoryName}&skip=${pageParam}`);
@@ -15,15 +16,14 @@ const getBoards = async (pageParam = 1, categoryName: string): Promise<BoardWith
   return res.json();
 };
 
-const BoardPage = ({ searchParams }: { searchParams?: { category: string } }) => {
-  const { boardsInfinite, hasNextPage, setSpinnerElement, refetch } = useInfiniteScroll({
-    fetcher: (pageParam = 1) => getBoards(pageParam, searchParams?.category || 'free'),
-    key: `${searchParams?.category || 'free'}list`,
-  });
+const BoardPage = () => {
+  const searchParams = useSearchParams();
+  const category = searchParams.get('category');
 
-  useEffect(() => {
-    refetch();
-  }, [searchParams?.category, refetch]);
+  const { boardsInfinite, hasNextPage, setSpinnerElement, refetch } = useInfiniteScroll({
+    fetcher: (pageParam = 1) => getBoards(pageParam, category || 'free'),
+    key: `${category}list`,
+  });
 
   if (!boardsInfinite) {
     return (
@@ -35,7 +35,7 @@ const BoardPage = ({ searchParams }: { searchParams?: { category: string } }) =>
 
   return (
     <>
-      <h2 className='font-bold text-2xl mb-4'>{urlToTitle(searchParams?.category || 'free')}</h2>
+      <h2 className='font-bold text-2xl mb-4'>{urlToTitle(category || 'free')}</h2>
       <ul>{boardsInfinite.pages.map(boards => boards.map(board => <Item board={board} key={board.id} />))}</ul>
       {hasNextPage ? (
         <div ref={setSpinnerElement} className='flex justify-center py-4 my-2'>
